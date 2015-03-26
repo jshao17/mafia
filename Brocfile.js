66a3e6b2d065b9fb93d5a967b069f646f82ef88f
jshao <johnshao17@gmail.com>
2015-03-25 22:48:35 -0700

    update bootstrap paper

--- Brocfile.js
+++ Brocfile.js
@@ -21,7 +21,7 @@ var app = new EmberApp({
 // along with the exports of each module as its value.
 
 app.import('bower_components/bootstrap/dist/js/bootstrap.js');
-app.import('bower_components/paper-less/css/paper.css');
+app.import('vendor/paper.css');
 app.import('bower_components/font-awesome/css/font-awesome.css');
 
 var fontTree = pickFiles('bower_components/font-awesome/fonts', {
