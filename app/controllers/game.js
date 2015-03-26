96d7ce9601f960a4964c9b2957366d7d19c52cbd
jshao <johnshao17@gmail.com>
2015-03-25 18:36:07 -0700

    assign roles logic

--- app/controllers/game.js
+++ app/controllers/game.js
@@ -1,4 +1,5 @@
 import Ember from 'ember';
+import random from '../utils/random';
 
 export default Ember.Controller.extend({
   range: function(num) {
@@ -22,8 +23,38 @@ export default Ember.Controller.extend({
   possibleFrWitch: function() {
     return this.range(this.get('model.players.length') - this.get('model.frMafia') - this.get('model.frCop'));
   }.property('model.frMafia', 'model.frCop', 'model.frWitch', 'model.players'),
+  assignRole: function(role, roleName, count, players) {
+    while(count > 0) {
+      var player = players.objectAt(random(1, players.get('length')));
+      if (!player.get(roleName)) {
+        player.set(roleName, role);
+        --count;
+      }
+    }
+  },
   actions: {
     assignRoles: function() {
+      var players = this.get('model.players');
+      players.forEach(player => {
+        player.set('firstRole', null);
+        player.set('secondRole', null);
+      });
+
+      this.assignRole('mafia', 'firstRole', this.get('model.frMafia'), players);
+      this.assignRole('mafia', 'secondRole', this.get('model.srMafia'), players);
+      this.assignRole('detective', 'firstRole', this.get('model.frCop'), players);
+      this.assignRole('detective', 'secondRole', this.get('model.srCop'), players);
+      this.assignRole('witch', 'firstRole', this.get('model.frWitch'), players);
+
+      players.forEach(player => {
+        if (!player.get('firstRole')) {
+          player.set('firstRole', 'civilian');
+        }
+        if (!player.get('secondRole')) {
+          player.set('secondRole', 'civilian');
+        }
+        player.save();
+      });
     }
   }
 });
